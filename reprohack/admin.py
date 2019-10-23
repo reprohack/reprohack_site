e2d4a580de5a5181ec159a2e3ae0462bc67eadbe
annakrystalli <annakrystalli@googlemail.com>
2019-10-23 09:38:53 +0100

    register Event as LeafletGeoadmin

--- reprohack/admin.py
+++ reprohack/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
 
 # Register your models here.
 from .models import Event
+from .models import Paper
 
-admin.site.register(Event)
+admin.site.register(Event, LeafletGeoAdmin)
+admin.site.register(Paper)
