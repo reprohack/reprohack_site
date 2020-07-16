22e7e889c4f3b02f7b43d2f13e7ba4d5cbff5c1b
Dr Griffith Rees <griff.rees@gmail.com>
2020-07-16 22:50:56 +0100

    Shift reprohack_hub into git and previous site into backup/.

---
+++ reprohack_hub/reprohack_hub/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from reprohack_hub.users.models import User
+from reprohack_hub.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
