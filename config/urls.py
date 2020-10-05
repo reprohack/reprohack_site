e977c45d8b0154beb78072b0c3e1c291064f94b1
Dr Griffith Rees <griff.rees@gmail.com>
2020-10-05 17:15:04 +0100

    Remove unnecessary import of TemplateView in config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
