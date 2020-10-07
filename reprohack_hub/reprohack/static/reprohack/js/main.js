b9dd2783d3d82a7a131418994b371e6c63b84a9a
Dr Griffith Rees <griff.rees@gmail.com>
2020-10-07 16:59:35 +0100

    Fix formatting within reprohack_hub.

--- reprohack_hub/reprohack/static/reprohack/js/main.js
+++ reprohack_hub/reprohack/static/reprohack/js/main.js
@@ -90,11 +90,11 @@ autoplaySpeed: 800,
     $(this).addClass('active');
     event.preventDefault();
 	});
-  
+
   // wow js
   new WOW().init();
 
-  // counter 
+  // counter
   $('.counter').counterUp({
     delay: 10,
     time: 10000
@@ -290,7 +290,7 @@ $(document).ready(function() {
 
 
 
-//------- Mailchimp js --------//  
+//------- Mailchimp js --------//
 function mailChimp() {
   $('#mc_embed_signup').find('form').ajaxChimp();
 }
@@ -314,4 +314,4 @@ mailChimp();
             $("#search_input").focus();
         });
 
-})(jQuery);	
+})(jQuery);
