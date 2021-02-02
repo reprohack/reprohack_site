ffcb9d38ffe40b33827cd72418c688f414978d9b
Anna Krystalli <annakrystalli@googlemail.com>
2021-02-02 16:52:46 +0200

    add signup submission button

--- reprohack_hub/users/forms.py
+++ reprohack_hub/users/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import forms, get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
 
 User = get_user_model()
 
@@ -28,3 +30,9 @@ class UserCreationForm(forms.UserCreationForm):
             return username
 
         raise ValidationError(self.error_messages["duplicate_username"])
+    
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Sign up'))
