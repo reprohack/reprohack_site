ea39ccf00f0403bb44b020ab37a145553d7c3e27
Anna Krystalli <annakrystalli@googlemail.com>
2021-02-04 12:03:27 +0200

    fix linting warning

--- reprohack_hub/users/forms.py
+++ reprohack_hub/users/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth import forms, get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, Submit
+from crispy_forms.layout import Submit
 
 User = get_user_model()
 
@@ -19,6 +19,7 @@ class UserCreationForm(forms.UserCreationForm):
     )
 
     class Meta(forms.UserCreationForm.Meta):
+        fields = ('username', 'email', 'password1', 'password2')
         model = User
 
     def clean_username(self):
@@ -30,9 +31,15 @@ class UserCreationForm(forms.UserCreationForm):
             return username
 
         raise ValidationError(self.error_messages["duplicate_username"])
-    
+
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Sign up'))
+        self.helper.layout = Layout(
+            # HTML('<h2>ReproHack Author Feedback Form</h2>'),
+            'username',
+            'email',
+            'password1',
+            'password2')
