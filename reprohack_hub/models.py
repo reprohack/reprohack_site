4ae5ae12c07df0d293d5d2c9e72cce460327a1a6
Twin Karmakharm <hi@twin.uk.com>
2021-09-30 23:40:34 +0100

    Overidden user e-mail field, made field mandatory

--- reprohack_hub/models.py
+++ reprohack_hub/models.py
@@ -73,6 +73,7 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    email = CharField(_("E-mail"), blank=False, max_length=255)
     bio = models.TextField(max_length=500, blank=True)
     affiliation = models.CharField(max_length=70, blank=True)
     location = models.CharField(max_length=70, blank=True)
