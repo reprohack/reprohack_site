cc60030a981aabe61e5858b8ae5bb8ec4aa7a414
Anna Krystalli <annakrystalli@googlemail.com>
2021-12-01 13:29:27 +0000

    add hackpad template url helptext

--- reprohack_hub/models.py
+++ reprohack_hub/models.py
@@ -23,6 +23,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from django.utils.safestring import mark_safe
 from django.db import models
 from django.contrib.auth.models import AbstractUser, AnonymousUser
 from django.db.models import CharField
@@ -176,7 +177,8 @@ class Event(models.Model):
     # location
     # venue = models.ForeignKey(Venue, on_delete=models.SET_NULL, null=True)
     description = MarkdownxField(_('Feel free to customise provided template'),
-                                 default=get_default_description)
+                                 default=get_default_description, 
+                                 help_text="Markdown field. See <a href='https://daringfireball.net/projects/markdown/syntax'> full documentation of supported syntax </a>." )
     # location = models.CharField(max_length=200)  # Location name?
     address1 = models.CharField(max_length=200, blank=True)
     address2 = models.CharField(max_length=200, blank=True)
